6dcb3076b8ff4432a97ec030ec991aef8d95f605
Aaron van Meerten <aaron.van.meerten@gmail.com>
2024-11-19 11:33:33 -0600

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-
-docker buildx build --no-cache --platform=linux/arm64,linux/amd64 --push --pull --progress=plain --tag aaronkvanmeerten/ops-agent:$TAG --tag aaronkvanmeerten/ops-agent:latest .
+[ -z "$REPO" ] && REPO="ops-prod-us-phoenix-1-registry.jitsi.net"
+# if the tag is not defined grab the current git short hash
+[ -z "$TAG" ] && TAG="$(git rev-parse --short HEAD)"
+docker buildx build --no-cache --platform=linux/arm64,linux/amd64 --push --pull --progress=plain --tag $REPO/jaas-test-wh-proxy:$TAG --tag $REPO/jaas-test-wh-proxy:latest .
