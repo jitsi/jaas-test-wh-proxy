d7e77f878edba73c759754b2e2ec657c6943bdfd
damencho <damencho@jitsi.org>
2025-02-28 12:47:52 -0600

    feat: adds debug log when building.

--- build/build.sh
+++ build/build.sh
@@ -3,4 +3,5 @@
 [ -z "$REPO" ] && REPO="ops-prod-us-phoenix-1-registry.jitsi.net"
 # if the tag is not defined grab the current git short hash
 [ -z "$TAG" ] && TAG="$(git rev-parse --short HEAD)"
+echo "Building $REPO/jaas-test-wh-proxy:$TAG"
 docker buildx build --no-cache --platform=linux/arm64,linux/amd64 --push --pull --progress=plain --tag $REPO/jaas-test-wh-proxy:$TAG --tag $REPO/jaas-test-wh-proxy:latest .
